af7d61c620dc31adf8712c10b9fafa30c5645329
kiermasp <pkierm@gmail.com>
2025-04-29 12:53:31 +0200

    Logger

--- custom_components/homebot_components/__init__.py
+++ custom_components/homebot_components/__init__.py
@@ -3,19 +3,10 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    ATTR_ENTITY_ID,
-    SERVICE_TURN_OFF,
-    SERVICE_TURN_ON,
-    STATE_ON,
-)
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import entity_registry
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.event import async_track_state_change_event
+
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 from .blind_controller import setup_blind_control
@@ -25,6 +16,9 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up External Blinds from a config entry."""
+    
+    _LOGGER.info("Setting up HomeBot Components")
+
     hass.data.setdefault(DOMAIN, {})
 
     # Store the configuration
@@ -38,5 +32,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+
+    _LOGGER.info("Setting async_unload_entry")
+    
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
